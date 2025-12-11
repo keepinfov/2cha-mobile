9de43c1293e29a530b0480a5c57b0a0b76d29ac1
Keepinfov <218463407+keepinfov@users.noreply.github.com>
2025-12-11 22:57:58 +0300

    Add dependencies for view tree owners

--- app/src/main/java/dev/yaul/twocha/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/dev/yaul/twocha/ui/fragments/SettingsFragment.kt
@@ -6,6 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.lifecycle.ViewTreeLifecycleOwner
+import androidx.savedstate.ViewTreeSavedStateRegistryOwner
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import dagger.hilt.android.AndroidEntryPoint
@@ -24,6 +27,9 @@ class SettingsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         return ComposeView(requireContext()).apply {
+            ViewTreeLifecycleOwner.set(this, viewLifecycleOwner)
+            ViewTreeSavedStateRegistryOwner.set(this, viewLifecycleOwner)
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
             setContent {
                 val themeStyle = viewModel.themeStyle.collectAsState()
                 val dynamicColor = viewModel.dynamicColor.collectAsState()
